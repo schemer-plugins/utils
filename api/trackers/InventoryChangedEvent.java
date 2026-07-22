8999353548d76bfcd85742ace82a06281a95d187
Yuri Moens <yuri.moens@gmail.com>
2026-07-22 17:49:56 +0200

    Fix InventoryChangedEvent parameter order
    ID and amount were swapped.

--- api/trackers/InventoryChangedEvent.java
+++ api/trackers/InventoryChangedEvent.java
@@ -1,3 +1,3 @@
 package be.moens.schemer.utils.api.trackers;
 
-public record InventoryChangedEvent(InventoryEventType type, int amount, int id, String name) {}
+public record InventoryChangedEvent(InventoryEventType type, int id, int amount, String name) {}
